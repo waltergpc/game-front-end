000ce10074e2af0f3965fe0e86bcbaf83701a689
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-01-14 18:29:52 -0600

    introduce layout wrapper for all routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
+import Layout from './Components/Layout'
 import About from './Pages/About'
 import NotFound from './Pages/NotFound'
 import SignIn from './Pages/SignIn'
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<About />} />
-      <Route path='/sign-in' element={<SignIn />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path='/' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Layout>
   )
 }
 
