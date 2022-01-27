5a40b5b8183a7081311d9ce0b5f5be1e5b007010
Walter González-Plata Cortes <waltergpc@hotmail.com>
2022-01-27 10:08:21 -0600

    introduce hall of fame page and styling for dashboard

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useUser } from '../Context/UserContext'
 import { Link, Navigate } from 'react-router-dom'
+import '../PagesCss/DashboardStyles.css'
 
 const Dashboard = () => {
   const { user } = useUser()
@@ -8,18 +9,34 @@ const Dashboard = () => {
   if (!user) return <Navigate to='/home' />
 
   return (
-    <section id='user-card'>
-      <div id='avatar'>
-        <img src={user.avatar} alt={user.nickname} />
+    <section className='user-card' id='user-card'>
+      <div className='avatar-div' id='avatar'>
+        <img className='avatar-img' src={user.avatar} alt={user.nickname} />
       </div>
-      <div id='user-info'>
-        <h4 id='user-name'>Name: {user.name}</h4>
-        <h4 id='user-nickname'>Nickname: {user.nickname}</h4>
-        <h4 id='user-id'>Player Id: {user.playerId}</h4>
-        <h4 id='user-status'>Status: {user.status}</h4>
-        <h4 id='user-ranking'>Ranking: {user.ranking}</h4>
+      <div className='user-info' id='user-info'>
+        <h4 className='field user-name' id='user-name'>
+          <span className='spec-name'>Name:</span>
+          <span>{user.name}</span>
+        </h4>
+        <h4 className='field user-nickname' id='user-nickname'>
+          <span className='spec-name'>Nickname:</span>
+          <span>{user.nickname}</span>
+        </h4>
+        <h4 className='field user-status' id='user-status'>
+          <span className='spec-name'>Status:</span> <span>{user.status}</span>
+        </h4>
+        <h4 className='field user-ranking' id='user-ranking'>
+          <span className='spec-name'>Ranking:</span>
+          <span>{user.ranking}</span>
+        </h4>
+        <h4 className='user-id' id='user-id'>
+          <div className='spec-name'>Player Id:</div>
+          <span>{user.playerId}</span>
+        </h4>
       </div>
-      <Link to='/update-user'>Update your user here</Link>
+      <Link className='to-update' to='/update-user'>
+        Update your user here
+      </Link>
     </section>
   )
 }
