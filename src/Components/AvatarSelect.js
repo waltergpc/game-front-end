965beaa305e3a2482c14e687eabc8e31e7e09df4
Ulises Peña <marculises14@gmail.com>
2022-01-27 16:50:47 -0600

    Update Profile page ready

--- src/Components/AvatarSelect.js
+++ src/Components/AvatarSelect.js
@@ -1,33 +1,36 @@
-import React from 'react'
-import { avatarOptions } from '../Utils/AvatarOptions'
-import './AvatarSelectStyles.css'
+import React from "react";
+import { avatarOptions } from "../Utils/AvatarOptions";
+
+import "./AvatarSelectStyles.css";
 
 const AvatarSelect = ({ setUpdatedUser, updatedUser }) => {
   const handleClick = (e) => {
-    setUpdatedUser({ ...updatedUser, avatar: e.target.id })
-  }
+    setUpdatedUser({ ...updatedUser, avatar: e.target.id });
+  };
 
   return (
-    <div className='avatar-select-div' id='avatar-select-div'>
-      {avatarOptions.map((avatar) => {
-        const { src, alias } = avatar
-        return (
-          <article
-            className={
-              updatedUser.avatar === src
-                ? 'avatar-option current'
-                : 'avatar-option'
-            }
-            key={alias}
-            id={src}
-            onClick={handleClick}
-          >
-            <img className='avatar-img' id={src} src={src} alt={alias} />
-          </article>
-        )
-      })}
+    <div className="avatar-select-div" id="avatar-select-div">
+      <div className="avatar-select-glass-div">
+        {avatarOptions.map((avatar) => {
+          const { src, alias } = avatar;
+          return (
+            <article
+              className={
+                updatedUser.avatar === src
+                  ? "avatar-option current"
+                  : "avatar-option"
+              }
+              key={alias}
+              id={src}
+              onClick={handleClick}
+            >
+              <img className="avatar-img" id={src} src={src} alt={alias} />
+            </article>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AvatarSelect
+export default AvatarSelect;
